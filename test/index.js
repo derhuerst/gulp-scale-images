90c34145d9c093f602bca507c8039f5b50ea7784
Jannis R <mail@jannisr.de>
2018-03-22 14:44:58 +0100

    adapt tests to 2f5bda5 :white_check_mark:

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const pifyTape = require('tape-promise').default
 const test = require('tape')
 const co = require('co')
+const pEvent = require('p-event')
 const vFile = require('vinyl-file')
 const Vinyl = require('vinyl')
 
@@ -26,41 +27,34 @@ const jpeg700 = {
 const isObj = o => o !== null && 'object' === typeof o && !Array.isArray(o)
 const pTest = pifyTape(test)
 
-test('throws on invalid usage', (t) => {
+pTest('emits erros on invalid input', co.wrap(function* (t) {
+	const plugin = createPlugin()
+	const writeImmediate = d => setImmediate(() => plugin.write(d))
+
 	const valid = {
 		maxWidth: 500,
 		maxHeight: 500,
 		format: 'jpeg',
 		withoutEnlargement: false
 	}
+
 	const invalid1 = Object.assign({}, valid, {maxWidth: 'foo'})
+	writeImmediate(invalid1)
+	t.ok(yield pEvent(plugin, 'error'))
+
 	const invalid2 = Object.assign({}, valid, {maxHeight: 'foo'})
+	writeImmediate(invalid2)
+	t.ok(yield pEvent(plugin, 'error'))
+
 	const invalid3 = Object.assign({}, valid, {format: 1})
+	writeImmediate(invalid3)
+	t.ok(yield pEvent(plugin, 'error'))
+
 	const invalid4 = Object.assign({}, valid, {withoutEnlargement: 'foo'})
+	writeImmediate(invalid4)
+	t.ok(yield pEvent(plugin, 'error'))
 
-	t.throws(() => createPlugin())
-	t.throws(() => createPlugin({}))
-	t.throws(() => createPlugin([]))
-	t.throws(() => createPlugin(['foo']))
-	t.throws(() => createPlugin([{}]))
-	t.throws(() => createPlugin([invalid1]))
-	t.throws(() => createPlugin([invalid2]))
-	t.throws(() => createPlugin([invalid3]))
-	t.throws(() => createPlugin([invalid4]))
-	t.throws(() => createPlugin([valid, invalid1]))
 	t.end()
-})
-
-pTest('emits error on invalid input', co.wrap(function* (t) {
-	const plugin = createPlugin([png500])
-	const invalidInput = {}
-
-	plugin.once('error', (err) => {
-		t.ok(err)
-		t.equal(err.file, invalidInput)
-		t.end()
-	})
-	plugin.end(invalidInput)
 }))
 
 pTest('skips directories', co.wrap(function* (t) {
@@ -85,8 +79,9 @@ pTest('skips directories', co.wrap(function* (t) {
 }))
 
 pTest('1 file, 500px png', co.wrap(function* (t) {
-	const plugin = createPlugin([png500])
+	const plugin = createPlugin()
 	const input = yield vFile.read(src)
+	input.scale = png500
 
 	plugin.on('data', (output) => {
 		t.ok(isObj(output), 'no file')
@@ -116,39 +111,45 @@ pTest('1 file, 500px png', co.wrap(function* (t) {
 }))
 
 pTest('1 file, 500px png, 700px jpeg', co.wrap(function* (t) {
-	const plugin = createPlugin([png500, jpeg700])
+	const plugin = createPlugin()
 
-	let hasPng = false, hasJpeg = false
-	plugin.on('data', (output) => {
-		t.ok(isObj(output), 'no file')
+	const in1 = yield vFile.read(src)
+	in1.scale = png500
+	const in2 = yield vFile.read(src)
+	in2.scale = jpeg700
 
-		t.ok(Buffer.isBuffer(output.contents), 'output.contents must be a buffer')
-		t.ok(output.contents.byteLength > 0, 'output.contents must be filled')
+	setImmediate(() => plugin.write(in1))
+	const out1 = yield pEvent(plugin, 'data')
 
-		const info = output[SHARP_INFO]
-		t.ok(isObj(info))
-		t.equal(typeof info.size, 'number')
-		t.ok(info.size >= 0)
+	setImmediate(() => plugin.end(in2))
+	const out2 = yield pEvent(plugin, 'data')
 
-		// image is landscape, so width will be maxed out
-		if (info.format === 'png') {
-			if (hasPng) t.fail('png emitted twice')
-			hasPng = true
-			t.equal(info.width, png500.maxWidth)
-			t.equal(typeof info.height, 'number')
-			t.ok(info.height <= png500.maxHeight)
-		} else if (info.format === 'jpeg') {
-			if (hasJpeg) t.fail('jpeg emitted twice')
-			hasJpeg = true
-			t.equal(info.width, jpeg700.maxWidth)
-			t.equal(typeof info.height, 'number')
-			t.ok(info.height <= jpeg700.maxHeight)
-		} else t.fail('invalid format ' + info.format)
+	t.ok(isObj(out1), 'no file')
+	t.ok(isObj(out2), 'no file')
 
-		if (hasPng && hasJpeg) t.end()
-	})
+	t.ok(Buffer.isBuffer(out1.contents), 'out1.contents must be a buffer')
+	t.ok(out1.contents.byteLength > 0, 'out1.contents must be filled')
+	t.ok(Buffer.isBuffer(out2.contents), 'out2.contents must be a buffer')
+	t.ok(out2.contents.byteLength > 0, 'out2.contents must be filled')
 
-	plugin.end(yield vFile.read(src)) // write input
+	const info1 = out1[SHARP_INFO]
+	t.ok(isObj(info1))
+	t.equal(typeof info1.size, 'number')
+	t.ok(info1.size >= 0)
+	const info2 = out2[SHARP_INFO]
+	t.ok(isObj(info2))
+	t.equal(typeof info2.size, 'number')
+	t.ok(info2.size >= 0)
+
+	// image is landscape, so width will be maxed out
+	t.equal(info1.width, png500.maxWidth)
+	t.equal(typeof info1.height, 'number')
+	t.ok(info1.height <= png500.maxHeight)
+	t.equal(info2.width, jpeg700.maxWidth)
+	t.equal(typeof info2.height, 'number')
+	t.ok(info2.height <= jpeg700.maxHeight)
+
+	t.end()
 }))
 
 // todo: >1 files
