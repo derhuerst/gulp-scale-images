90c8a6adc502d7ab22f53635ac7cef4b6304433f
Jannis R <mail@jannisr.de>
2018-03-22 02:05:54 +0100

    obey plugin guidelines :bug:
    - prefix errors with pkg name
    - emit error on streaming files

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 'use strict'
 
+const PluginError = require('plugin-error')
 const through = require('through2')
 const eachSeries = require('async/eachSeries')
 
 const resize = require('./lib/resize')
+const pkgName = require('./package.json').name
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 const isObj = o => o !== null && 'object' === typeof o && !Array.isArray(o)
 
+const createErr = (msg, file) => {
+	const err = new PluginError(pkgName, {message: msg})
+	if (file) err.file = file
+	return err
+}
+
 const validateConfigs = (cfgs) => {
-	if (!Array.isArray(cfgs)) throw new Error('configs must be an array')
-	if (!cfgs.length) throw new Error('configs is empty')
+	if (!Array.isArray(cfgs)) throw createErr('configs must be an array')
+	if (!cfgs.length) throw createErr('configs is empty')
 	for (let cfg of cfgs) {
-		if (!isObj(cfg)) throw new Error('cfg must be an object')
+		if (!isObj(cfg)) throw createErr('cfg must be an object')
 		if ('number' !== typeof cfg.maxWidth) {
-			throw new Error('cfg.maxWidth must be a number')
+			throw createErr('cfg.maxWidth must be a number')
 		}
 		if ('number' !== typeof cfg.maxHeight) {
-			throw new Error('cfg.maxHeight must be a number')
+			throw createErr('cfg.maxHeight must be a number')
 		}
 		if (cfg.format && 'string' !== typeof cfg.format) {
-			throw new Error('cfg.format must be a string')
+			throw createErr('cfg.format must be a string')
 		}
 		if (cfg.withoutEnlargement && 'boolean' !== typeof cfg.withoutEnlargement) {
-			throw new Error('cfg.withoutEnlargement must be a boolean')
+			throw createErr('cfg.withoutEnlargement must be a boolean')
 		}
 	}
 }
@@ -34,15 +42,15 @@ const createScaleImagesPlugin = (configs) => {
 
 	const out = through.obj(function processFile(input, _, cb) {
 		if (!input || 'function' !== typeof input.isDirectory) {
-			const err = new Error('invalid vinyl file passed')
-			err.file = input
-			out.emit('error', err)
-			cb()
-			return
+			out.emit('error', createErr('invalid vinyl file passed', input))
+			return cb()
+		}
+		if (input.isStream()) {
+			out.emit('error', createErr('streaming files are not supported', input))
+			return cb()
 		}
 		if (input.isDirectory()) { // ignore directories
-			cb()
-			return
+			return cb()
 		}
 
 		const self = this
