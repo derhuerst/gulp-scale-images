1e73c7b8effaca7a8fd4e6dca5b3e66f8a2a5f96
Jannis R <mail@jannisr.de>
2018-03-22 01:39:07 +0100

    implementation :sparkles:, reverse enlargement flag

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 'use strict'
 
-// todo
+const through = require('through2')
+const eachSeries = require('async/eachSeries')
+
+const resize = require('./lib/resize')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const isObj = o => o !== null && 'object' === typeof o && !Array.isArray(o)
+
+const validateConfigs = (cfgs) => {
+	if (!Array.isArray(cfgs)) throw new Error('configs must be an array')
+	if (!cfgs.length) throw new Error('configs is empty')
+	for (let cfg of cfgs) {
+		if (!isObj(cfg)) throw new Error('cfg must be an object')
+		if ('number' !== typeof cfg.maxWidth) {
+			throw new Error('cfg.maxWidth must be a number')
+		}
+		if ('number' !== typeof cfg.maxHeight) {
+			throw new Error('cfg.maxHeight must be a number')
+		}
+		if (cfg.format && 'string' !== typeof cfg.format) {
+			throw new Error('cfg.format must be a string')
+		}
+		if (cfg.withoutEnlargement && 'boolean' !== typeof cfg.withoutEnlargement) {
+			throw new Error('cfg.withoutEnlargement must be a boolean')
+		}
+	}
+}
+
+const createScaleImagesPlugin = (configs) => {
+	if (!isProduction) validateConfigs(configs)
+
+	const out = through.obj(function processFile(input, _, cb) {
+		const self = this
+		const processConfig = (cfg, cb) => {
+			resize(input, cfg, (err, output) => {
+				if (err) return cb(err)
+				self.push(output)
+				cb()
+			})
+		}
+
+		eachSeries(configs, processConfig, (err) => {
+			if (err) {
+				out.emit('error', err)
+				cb(null)
+				out.destroy()
+			} else cb()
+		})
+	})
+
+	return out
+}
+
+createScaleImagesPlugin.SHARP_INFO = resize.SHARP_INFO
+module.exports = createScaleImagesPlugin
