81927af5f5433cae67413b2a9dd61a50de9ab772
Jannis R <mail@jannisr.de>
2018-03-22 16:08:56 +0100

    bugfix :bug:, refactor SHARP_INFO

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PluginError = require('plugin-error')
 const through = require('through2')
 
 const resize = require('./lib/resize')
+const SHARP_INFO = require('./lib/sharp-info')
 const pkgName = require('./package.json').name
 
 const isObj = o => o !== null && 'object' === typeof o && !Array.isArray(o)
@@ -25,19 +26,19 @@ const createScaleImagesPlugin = () => {
 
 		const s = input.scale
 		if (!isObj(s)) {
-			return emitErr('file.scale must be an object')
+			return onErr('file.scale must be an object')
 		}
 		if ('number' !== typeof s.maxWidth) {
-			return emitErr('file.scale.maxWidth must be a number')
+			return onErr('file.scale.maxWidth must be a number')
 		}
 		if ('number' !== typeof s.maxHeight) {
-			return emitErr('file.scale.maxHeight must be a number')
+			return onErr('file.scale.maxHeight must be a number')
 		}
 		if (s.format && 'string' !== typeof s.format) {
-			return emitErr('file.scale.format must be a string')
+			return onErr('file.scale.format must be a string')
 		}
 		if (s.withoutEnlargement && 'boolean' !== typeof s.withoutEnlargement) {
-			return emitErr('file.scale.withoutEnlargement must be a boolean')
+			return onErr('file.scale.withoutEnlargement must be a boolean')
 		}
 
 		const self = this
@@ -51,5 +52,5 @@ const createScaleImagesPlugin = () => {
 	return out
 }
 
-createScaleImagesPlugin.SHARP_INFO = resize.SHARP_INFO
+createScaleImagesPlugin.SHARP_INFO = SHARP_INFO
 module.exports = createScaleImagesPlugin
